f3872a162417f94bac06d8450c41af8553cacb4c
Jam Risser <jam@codejam.ninja>
2020-07-09 22:19:19 +0000

    Improved mutex

--- src/hooks/useStateCache.ts
+++ src/hooks/useStateCache.ts
@@ -11,7 +11,7 @@ import useStateCacheConfig from './useStateCacheConfig';
 export default function useStateCache<T>(
   key: number | string | (number | string | void | null)[],
   initialState: T,
-  reconcile?: (cachedState: T, newState: T) => T
+  reconcile?: (prevState: T, nextState: T) => T
 ): [T | undefined, Dispatch<SetStateAction<T>>, boolean] {
   const [delayedStates, setDelayedStates] = useState<T[]>([]);
   const {
@@ -35,9 +35,12 @@ export default function useStateCache<T>(
   ): string | undefined {
     if (Array.isArray(key)) {
       if (
-        key.find(
-          (keyItem: null | number | string | void) =>
-            typeof keyItem === 'undefined' || keyItem === null
+        key.reduce(
+          (voidKey: boolean, keyItem: null | number | string | void) => {
+            if (typeof keyItem !== 'number' && !keyItem) voidKey = true;
+            return voidKey;
+          },
+          false
         )
       ) {
         return undefined;
@@ -48,81 +51,82 @@ export default function useStateCache<T>(
     return [namespace, key].join('/');
   }
 
-  const reconcileDelayedState = useCallback(
-    (newState: T, delayedStates: T[]): T => {
-      if (reconcile && delayedStates.length) {
-        newState = delayedStates.reduce(
-          (newState: T, delayedState: T) => reconcile(newState, delayedState),
-          newState
-        );
-      }
-      setDelayedStates([]);
-      return newState;
-    },
-    []
-  );
-
-  const resolveStateAction = useCallback(
-    (setStateAction: SetStateAction<T>, prevState: T | undefined): T => {
-      return (setStateAction as (prevState: T | undefined) => T)(prevState);
-    },
-    []
-  );
+  function resolveStateAction(
+    setStateAction: SetStateAction<T>,
+    prevState: T | undefined
+  ): T {
+    return (setStateAction as (prevState: T | undefined) => T)(prevState);
+  }
 
   useEffect(() => {
     setCalculatedKey(calculateKey(key));
   }, [key]);
 
   useEffect(() => {
+    if (!enabled || !calculatedKey) return;
     (async () => {
-      if (!enabled || !calculatedKey) return;
-      setMutex(false);
       try {
         const cachedState = JSON.parse(
           await AsyncStorage.getItem(calculatedKey)
         );
-        if (typeof cachedState !== 'undefined' && cachedState !== null) {
-          setState(cachedState);
-        } else {
+        if (typeof cachedState === 'undefined' || cachedState === null) {
           setState(initialState);
+        } else {
+          setState(cachedState);
         }
       } catch (err) {
         setState(initialState);
       }
+      setMutex(false);
     })();
-  }, []);
+  }, [calculatedKey]);
 
-  function handleSetState(setStateAction: SetStateAction<T>): void {
-    if (!calculatedKey) return;
-    if (mutex) {
-      if (reconcile) {
-        let delayedState = setStateAction as T;
-        if (typeof setStateAction === 'function') {
-          delayedState = resolveStateAction(setStateAction, state);
-        }
-        setDelayedStates([...delayedStates, delayedState]);
-      } else {
-        const err = new Error('cannot set state while mutex locked');
-        if (strict) {
-          throw err;
-        } else if (!silence) {
-          console.warn(err);
-        }
-      }
-      return;
-    }
-    if (typeof setStateAction === 'function') {
-      return setState((prevState: T | undefined) => {
-        let newState = resolveStateAction(setStateAction, prevState);
-        newState = reconcileDelayedState(newState, delayedStates);
-        if (enabled) AsyncStorage.setItem(calculatedKey, JSON.stringify(state));
-        return newState;
-      });
-    }
-    let newState = setStateAction as T;
-    newState = reconcileDelayedState(newState, delayedStates);
-    if (enabled) AsyncStorage.setItem(calculatedKey, JSON.stringify(state));
+  useEffect(() => {
+    if (mutex || !reconcile || !delayedStates.length) return;
+    const newState = delayedStates.reduce(
+      (prevState: T, nextState: T) => reconcile(prevState, nextState),
+      state!
+    );
+    setDelayedStates([]);
+    if (enabled) AsyncStorage.setItem(calculatedKey!, JSON.stringify(newState));
     return setState(newState);
-  }
+  }, [mutex, delayedStates]);
+
+  const handleSetState = useCallback(
+    (setStateAction: SetStateAction<T>): void => {
+      if (mutex) {
+        if (reconcile) {
+          let delayedState = setStateAction as T;
+          if (typeof setStateAction === 'function') {
+            delayedState = resolveStateAction(setStateAction, state);
+          }
+          setDelayedStates([...delayedStates, delayedState]);
+        } else {
+          const err = new Error('cannot set state while mutex locked');
+          if (strict) {
+            throw err;
+          } else if (!silence) {
+            console.warn(err);
+          }
+        }
+        return;
+      }
+      if (typeof setStateAction === 'function') {
+        return setState((prevState: T | undefined) => {
+          const newState = resolveStateAction(setStateAction, prevState);
+          if (enabled) {
+            AsyncStorage.setItem(calculatedKey!, JSON.stringify(newState));
+          }
+          return newState;
+        });
+      }
+      const newState = setStateAction as T;
+      if (enabled)
+        AsyncStorage.setItem(calculatedKey!, JSON.stringify(newState));
+      return setState(newState);
+    },
+    [mutex, state]
+  );
+
   return [state, handleSetState, mutex];
 }
